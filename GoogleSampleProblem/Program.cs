69a83e485d9804a8a1b70109a0573a4c1a3b10f6
5enthilbaliah <ternaryoperator.fork@gmail.com>
2024-03-09 15:37:49 -0600

    Initial commit

---
+++ GoogleSampleProblem/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var result = HasPairMatchingSum (new int [] { 1, 2, 3, 4, 5, 6 }, 8 );
+Console.WriteLine(result);
+return;
+
+
+bool HasPairMatchingSum(IEnumerable<int> arr, int sum)
+{
+    var compliments = new HashSet<int>();
+    foreach (var num in arr)
+    {
+        if (compliments.Contains(num))
+            return true;
+
+        compliments.Add(sum - num);
+    }
+    
+    return false;
+}
