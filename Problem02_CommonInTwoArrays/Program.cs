96e6046732f3307bffc4c3e59325847355eda215
5enthilbaliah <ternaryoperator.fork@gmail.com>
2024-03-09 16:48:13 -0600

    Common in two arrays

---
+++ Problem02_CommonInTwoArrays/Program.cs
@@ -0,0 +1,61 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// O(n^2)
+bool HasCommonData(char[] arr1, char[] arr2)
+{
+    foreach (var a1 in arr1)
+    {
+        foreach (var a2 in arr2)
+        {
+            if (a1 == a2)
+                return true;
+        }
+    }
+
+    return false;
+}
+
+// O(n)
+bool HasCommonDataOptimized(char [] arr1, char [] arr2)
+{
+    var map = new HashSet<char>();
+
+    char [] shorter;
+    char[] current;
+
+    if (arr1.Length < arr2.Length)
+    {
+        shorter = arr1;
+        current = arr2;
+    }
+    else
+    {
+        shorter = arr2;
+        current = arr1;
+    }
+    
+    for (var i = 0; i < shorter.Length; i++)
+    {
+        if (!map.Contains(shorter[i]))
+            map.Add(shorter[i]);
+
+        if (map.Contains(current[i]))
+            return true;
+    }
+
+    for (var i = shorter.Length; i < current.Length; i++)
+    {
+        if (map.Contains(current[i]))
+            return true;
+    }
+
+    return false;
+}
+
+Console.WriteLine("Brute-Force O(n^2) :");
+Console.WriteLine(HasCommonData(new []{ 'a', 'b', 'c', 'x'}, new []{ 'z', 'y', 'x'}));
+Console.WriteLine(HasCommonData(new []{ 'a', 'b', 'c', 'x'}, new []{ 'z', 'y', 'i'}));
+
+Console.WriteLine("Optimized O(n) :");
+Console.WriteLine(HasCommonDataOptimized(new []{ 'a', 'b', 'c', 'x'}, new []{ 'z', 'y', 'x'}));
+Console.WriteLine(HasCommonDataOptimized(new []{ 'a', 'b', 'c', 'x'}, new []{ 'z', 'y', 'i'}));
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# DatastrucutesAndAlgorithms
+# Datastructures and Algorithms
+
+We are starting with the following sample problems.
+
+## Problems
+- Google sample - Find if a pair exists in an array
+- Find is there are any common item in two arrays
+
