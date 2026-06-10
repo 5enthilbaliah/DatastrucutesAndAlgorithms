ebfd121de9522177ec630cad9cac6adf37981f86
5enthilbaliah <ternaryoperator.fork@gmail.com>
2026-06-10 18:09:56 -0500

    Nth root

---
+++ Problem13_NthRoot/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine($"3rd root of 8 is {FindNthRoot(3, 8)}");
+Console.WriteLine($"3rd root of 9 is {FindNthRoot(3, 9)}");
+Console.WriteLine($"4th root of 16 is {FindNthRoot(4, 16)}");
+return;
+
+int FindNthRoot(int n, int m)
+{
+    if (m == 1)
+        return 1;
+    
+    for (var i = 2; i <= m / 2; i++)
+    {
+        var product = 1;
+
+        for (int j = 0; j < n; j++)
+        {
+            product *= i;
+            if (product > m)
+                break;
+        }
+        
+        if (product == m)
+            return i;
+    }
+
+    return -1;
+}
