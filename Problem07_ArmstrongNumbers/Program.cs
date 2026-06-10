55e65b13a8f75d605e0f0e817fd145cd0fd367ea
5enthilbaliah <ternaryoperator.fork@gmail.com>
2026-06-10 13:52:06 -0500

    Armstrong Numbers

---
+++ Problem07_ArmstrongNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.WriteLine($"Is 153 an Armstrong number? {CheckIfArmstrongNumber(153)}");
+Console.WriteLine($"Is 372 an Armstrong number? {CheckIfArmstrongNumber(372)}");
+
+
+bool CheckIfArmstrongNumber(int n)
+{
+    var number = n;
+    var armstrongCount = 0;
+    do
+    {
+        var reminder = number % 10;
+        armstrongCount += reminder * reminder * reminder;
+        number /= 10;
+    } 
+    while (number > 0);
+
+    return armstrongCount == n;
+}
