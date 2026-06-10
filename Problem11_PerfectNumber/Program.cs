e0f0784d0ae17a2df6268a259ac81ca947cdf45d
5enthilbaliah <ternaryoperator.fork@gmail.com>
2026-06-10 16:29:30 -0500

    Perfect number

---
+++ Problem11_PerfectNumber/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine($"6 is a perfect number? {PerfectNumber(6)}");
+Console.WriteLine($"10 is a perfect number? {PerfectNumber(10)}");
+
+return;
+
+
+bool PerfectNumber(int n)
+{
+    var sum = 0;
+    for (int i = 1; i < n; i++)
+    {
+        if (n % i == 0)
+            sum += i;
+    }
+    
+    return sum == n;
+}
