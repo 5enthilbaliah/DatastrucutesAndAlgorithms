7c3139238a804a1710db79864fe741998bc7584b
5enthilbaliah <ternaryoperator.fork@gmail.com>
2026-06-10 13:57:44 -0500

    Reverse number

---
+++ Problem08_ReverseNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine($"Reverse of 122 is {ReverseNumber(122)}");
+Console.WriteLine($"Reverse of 200 is {ReverseNumber(200)}");
+return;
+
+int ReverseNumber(int n)
+{
+    var reverse = 0;
+    var remaining = n;
+    do
+    {
+        var remainder = remaining % 10;
+        reverse = reverse * 10 + remainder;
+
+        remaining /= 10;
+    } while (remaining > 0);
+    
+    return reverse;
+}
