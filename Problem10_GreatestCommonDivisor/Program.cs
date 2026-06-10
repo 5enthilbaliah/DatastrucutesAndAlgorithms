d3adb34025b82eeb3a3e6a2c47cc0a3789df2bc0
5enthilbaliah <ternaryoperator.fork@gmail.com>
2026-06-10 14:40:30 -0500

    GCD

---
+++ Problem10_GreatestCommonDivisor/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine($"GCD of 60 and 36 is {GreatestCommonDivisor(60, 36)}");
+Console.WriteLine($"GCD of 20 and 28 is {GreatestCommonDivisor(20, 28)}");
+
+return;
+
+int GreatestCommonDivisor(int a, int b)
+{
+    var abs_a = Math.Abs(a);
+    var abs_b = Math.Abs(b);
+    var isFirstGreater = abs_a > abs_b;
+    if (!isFirstGreater)
+        (abs_a, abs_b) = (abs_b, abs_a);
+
+    while (abs_b != 0)
+    {
+        (abs_a, abs_b) = (abs_b, abs_a % abs_b);
+    }
+    
+    
+    return abs_a;
+}
